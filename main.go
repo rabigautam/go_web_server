a97ebe2bc755b9e8e7d76b083a0b939c4f532eb1
Rabi Gautam <rabi@sharelookapp.com>
2022-04-20 12:55:57 +0545

    server listen and route implemented

--- main.go
+++ main.go
@@ -5,6 +5,27 @@ import (
 	"log"
 	"net/http"
 )
-func main(){
-	
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	port := 4040
+	fmt.Printf("Starting server at port %v\n", port)
+	fmt.Printf("go to the link: http://localhost:%v", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
