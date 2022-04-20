b0a4e4712bf33b1cc29bf0450a80098068b1ea90
Rabi Gautam <rabi@sharelookapp.com>
2022-04-20 13:42:19 +0545

    implement form handler

--- main.go
+++ main.go
@@ -16,10 +16,26 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
 }
 
+func formHandler(w http.ResponseWriter,r * http.Request){
+	if err:=r.ParseForm();err!=nil{
+		fmt.Fprintf(w,"Parse From err :%v",err)
+		return
+	}
+	fmt.Fprintf(w,"Post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w,"Name = %s\n",name)
+	fmt.Fprintf(w,"Address = %s\n",address)
+
+
+
+}
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
 	port := 4040
 	fmt.Printf("Starting server at port %v\n", port)
 	fmt.Printf("go to the link: http://localhost:%v", port)
